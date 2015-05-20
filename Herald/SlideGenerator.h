0f7ce51f8d47d4beb97df5f0b5dd34b343e9bb2b
Jason Terhorst <yourstruly@jterhorst.com>
2015-05-19 21:26:05 -0500

    WIP slide generator

--- Herald/SlideGenerator.h
+++ Herald/SlideGenerator.h
@@ -1,14 +1,15 @@
 //
-//  Document.h
+//  SlideGenerator.h
 //  Herald
 //
 //  Created by Jason Terhorst on 5/19/15.
 //  Copyright (c) 2015 Worship Kit. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-#import "BSManagedDocument.h"
+#import <Foundation/Foundation.h>
 
-@interface Document : BSManagedDocument
+@interface SlideGenerator : NSObject
+
+
 
 @end
--- Herald/SlideGenerator.m
+++ Herald/SlideGenerator.m
@@ -1,14 +1,13 @@
 //
-//  Document.h
+//  SlideGenerator.m
 //  Herald
 //
 //  Created by Jason Terhorst on 5/19/15.
 //  Copyright (c) 2015 Worship Kit. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-#import "BSManagedDocument.h"
+#import "SlideGenerator.h"
 
-@interface Document : BSManagedDocument
+@implementation SlideGenerator
 
 @end
